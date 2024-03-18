2b6127c6c8fd6d15878941a8a8e2e44e61c0165b
Vũ Hà Minh Trang <78261243+beckachuu@users.noreply.github.com>
2024-03-18 11:07:29 +0700

    fix: change concat layer in custom yolov8 head

--- src/model/fpcm.py
+++ src/model/fpcm.py
@@ -14,6 +14,7 @@ class FPCM(nn.Module):
 
         b, c, h, w = x.shape
         x = x.view(b, c, h*w)
+        device = x.device
 
         Freq = torch.fft.fft(torch.fft.fft(x.permute(0,2,1), dim=-1), dim=-2)
 
@@ -21,7 +22,7 @@ class FPCM(nn.Module):
         cutoff_freq = patches // 2 - (patches // 2 - patches // 8) * (self.epoch / self.epoch_lim)
         lowpass_filter_l = torch.exp(-0.5 * torch.square(torch.linspace(0, patches // 2 - 1, patches // 2).unsqueeze(1).repeat(1,c) / (cutoff_freq))).view(1, patches // 2, c)
         lowpass_filter_r = torch.flip(torch.exp(-0.5 * torch.square(torch.linspace(1, patches // 2 , patches // 2).unsqueeze(1).repeat(1,c) / (cutoff_freq))).view(1, patches // 2, c), [1])
-        lowpass_filter = torch.concat((lowpass_filter_l, lowpass_filter_r), dim=1)
+        lowpass_filter = torch.concat((lowpass_filter_l, lowpass_filter_r), dim=1).to(device)
 
         low_Freq = Freq * lowpass_filter
         lowFreq_feature = torch.fft.ifft(torch.fft.ifft(low_Freq, dim=-2), dim=-1).real
@@ -34,5 +35,4 @@ class FPCM(nn.Module):
         out = out.view(b, c, h, w)
 
         return out
-
 
